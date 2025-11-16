d35d677e2e1b456755ac0c6580ddb8f98d83575b
tanmay <tanmaysawaji9@gmail.com>
2025-11-16 10:13:32 +0530

    2 commit

--- src/landingpage/home/Hero.js
+++ src/landingpage/home/Hero.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src='homeHero.png' alt='Heroimage' className='mb-5'></img>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
+                <button className="p-3 fs-5" style={{width:"20%" , margin:"0 auto", backgroundColor:"blue", color:"white" ,borderRadius:"8px", border: "2px solid transparent"}}>Signup Now</button>
+            </div>
+        </div>
      );
 }
 
