531b328cde283065a388f8e295a7eb5b68e3ecdc
tanmay <tanmaysawaji9@gmail.com>
2025-11-27 01:35:39 +0530

    9 commit

--- src/landingpage/products/Universe.js
+++ src/landingpage/products/Universe.js
@@ -14,25 +14,25 @@ function Universe() {
           <img src="smallcaseLogo.png" />
           <p className="text-small text-muted">Thematic investment platform</p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 p-3 mt-4">
+          <img src="dittoLogo.png" style={{width:"200px"}} />
+          <p className="text-small text-muted" >Personalized advice on life and health insurance</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="goldenpiLogo.png" style={{width:"200px"}}/>
+          <p className="text-small text-muted">Bonds Treading Platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+          <img src="sensibullLogo.svg" style={{width:"250px"}}/>
+          <p className="text-small text-muted">Option Trading platform</p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 p-3 mt-3">
+          <img src="streakLogo.png" style={{width:"250px"}}/>
+          <p className="text-small text-muted">Systematic Treading platform</p>
         </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
+        <div className="col-4 p-3 mt-4">
+          <img src="zerodhaFundhouse.png" style={{width:"250px"}}/>
+          <p className="text-small text-muted">Asset Management venture</p>
         </div>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
